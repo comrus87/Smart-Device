cf00b713186b12a5837d11e0e7308d99f0839250
Andrew <nk-andrew1987@mail.ru>
2019-09-17 23:13:16 +0300

    Поправлены недочеты

--- build/js/script.js
+++ build/js/script.js
@@ -3,7 +3,12 @@
 const overlay = document.querySelector('.overlay-modal');
 const modal = document.querySelector('.modal-call');
 const buttonOpenModal = document.querySelector('.page-header__button');
-const buttonCloseModal = document.querySelector('.modal-call__image-close');
+const buttonCloseModal = document.querySelector('.modal-call__close');
+const navList = document.querySelector('.page-footer__nav-list');
+const contactList = document.querySelector('.page-footer__contacts-wrap');
+const buttonNavList = document.querySelectorAll('.page-footer__switch-button')[0];
+const buttonContactList = document.querySelectorAll('.page-footer__switch-button')[1];
+
 
 buttonOpenModal.addEventListener('click', function () {
     overlay.classList.remove('overlay-modal--close');
@@ -11,17 +16,29 @@ buttonOpenModal.addEventListener('click', function () {
     document.body.style.overflow = 'hidden';
 });
 
-
-// buttonCloseModal.addEventListener('click', function () {
-//     overlay.classList.remove('overlay-modal--open');
-//     overlay.classList.add('overlay-modal--close');
-//     document.body.style.overflow = '';
-// })
-
 overlay.addEventListener('click', function (evt) {
   if (!modal.contains(evt.target) || evt.target == buttonCloseModal) {
     overlay.classList.remove('overlay-modal--open');
     overlay.classList.add('overlay-modal--close');
     document.body.style.overflow = '';
   }
+})
+
+function removeNoJs (block) {
+  if (block.classList.contains('list--no-js')) {
+    block.classList.remove('list--no-js');
+  }
+}
+
+removeNoJs(navList);
+removeNoJs(contactList);
+
+buttonNavList.addEventListener('click', function () {
+    navList.classList.toggle('footer-list--closed');
+    this.classList.toggle('page-footer__nav--open');
+})
+
+buttonContactList.addEventListener('click', function () {
+    contactList.classList.toggle('footer-list--closed');
+    this.classList.toggle('page-footer__nav--open');
 })
--- source/js/script.js
+++ source/js/script.js
@@ -4,6 +4,11 @@ const overlay = document.querySelector('.overlay-modal');
 const modal = document.querySelector('.modal-call');
 const buttonOpenModal = document.querySelector('.page-header__button');
 const buttonCloseModal = document.querySelector('.modal-call__close');
+const navList = document.querySelector('.page-footer__nav-list');
+const contactList = document.querySelector('.page-footer__contacts-wrap');
+const buttonNavList = document.querySelectorAll('.page-footer__switch-button')[0];
+const buttonContactList = document.querySelectorAll('.page-footer__switch-button')[1];
+
 
 buttonOpenModal.addEventListener('click', function () {
     overlay.classList.remove('overlay-modal--close');
@@ -17,4 +22,23 @@ overlay.addEventListener('click', function (evt) {
     overlay.classList.add('overlay-modal--close');
     document.body.style.overflow = '';
   }
+})
+
+function removeNoJs (block) {
+  if (block.classList.contains('list--no-js')) {
+    block.classList.remove('list--no-js');
+  }
+}
+
+removeNoJs(navList);
+removeNoJs(contactList);
+
+buttonNavList.addEventListener('click', function () {
+    navList.classList.toggle('footer-list--closed');
+    this.classList.toggle('page-footer__nav--open');
+})
+
+buttonContactList.addEventListener('click', function () {
+    contactList.classList.toggle('footer-list--closed');
+    this.classList.toggle('page-footer__nav--open');
 })
